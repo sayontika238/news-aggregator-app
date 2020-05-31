aa318d00bd6355eb58d682a33b4d2e08a9948594
sayontika238 <konarsayontika560@gmail.com>
2020-05-31 20:03:44 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,4 @@
-const url ='http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-07&sortBy=publishedAt&apiKey=4e5acce35bab4b4d9c29883a8ba9ded3';
-
-
+const url =http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-30&sortBy=publishedAt&apiKey=4f403b4d39ba45b9948f3e95cdb92b2a
 
 $(document).ready(async function news(){
 	let response = await fetch(url);
@@ -32,13 +30,13 @@ $(document).ready(async function news(){
 			
             document.getElementById("news-articles").innerHTML=news;
         })
-       
+    
     
 
 $(document).ready(function(){ 
     $('#search').keypress(()=>{
       let searchField = $("#search").val();
-      const url1 = `https://newsapi.org/v2/everything?q=${searchField}&apiKey=4e5acce35bab4b4d9c29883a8ba9ded3`;
+      const url1 = http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-30&sortBy=publishedAt&apiKey=4f403b4d39ba45b9948f3e95cdb92b2a
       
       if(searchField !== ""){
         $.ajax({
@@ -92,3 +90,33 @@ $(document).ready(async function news(){
                        <p class="article-description">${article.description}</p>
                        <span class="article-author">${article.author}</span>
                        <a class="article-link" href="${article.url}" target="_blank"></a>
+...
+
+[Message clipped]  View entire message
+
+Sayontika Konar <konarsayontika560@gmail.com>
+Mon, May 25, 4:50 PM (6 days ago)
+to Meghapatil920
+
+
+const url ='http://newsapi.org/v2/everything?q=bitcoin&from=2020-04-07&sortBy=publishedAt&apiKey=4e5acce35bab4b4d9c29883a8ba9ded3';
+
+
+
+$(document).ready(async function news(){
+	let response = await fetch(url);
+	let data = await response.json();
+	//debugger;
+	let news = ''
+	data.articles.forEach(article=> {
+		// put your loop code over here 
+				news+= `
+                    <li class="article">
+                       <img class="article-img" src="${article.urlToImage}"/>
+                       <h2 class="article-title">${article.title}</h2>
+                       <p class="article-description">${article.description}</p>
+                       <span class="article-author">${article.author}</span>
+                       <a class="article-link" href="${article.url}" target="_blank"></a>
+                    </li>
+                `;
+<td id="gmail-LC25" class="gmail-blob-code gmail-blob-code-inner gmail-js-file-line" style="box
